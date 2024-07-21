486ce8fba4ca45a5889afe93e9463444911711c4
abeel2019029 <u2019029@giki.edu.pk>
2024-07-21 21:01:20 +0500

    Removed Projects.py

--- app/api_models/projects.py
+++ app/api_models/projects.py
@@ -3,15 +3,4 @@ from typing import Optional, List
 from datetime import datetime
 
 
-class ProjectCreate(BaseModel):
-    name: str
-    description: Optional[str] = None
-
-
-class ProjectResponse(ProjectCreate):
-    id: int
-    created_at: datetime
-    
-    class Config:
-        from_attributes = True
 
